dd5c576d026ebb94671886120ab3f6ddd1bf51eb
ds-mayur <mayur.rathod@devjisystems.com>
2019-10-04 21:56:28 +0300

    Initial Commit

---
+++ Camera/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Camera
+//
+//  Created by ds-mayur on 10/4/19.
+//  Copyright © 2019 Mayur Rathod. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
